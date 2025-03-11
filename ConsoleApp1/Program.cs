f0c0f6e396ae658ad6383a5f50cfbadc4ae9f1a8
Hubber773 <githubbr53@gmail.com>
2025-03-11 10:12:25 +0100

    konsolówka

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Podaj a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Podaj b: ");
+int b = int.Parse(Console.ReadLine());
+bool kontynuj = true;
+
+while (a != b & kontynuj == true)
+{
+    if (a >= b)
+    {
+        a -= b;
+        kontynuj = true;
+        //Console.WriteLine("a jest wieksze od b:      A:" + a + "  B: " + b);
+    }
+    else
+    {
+        b -= a;
+        kontynuj = true;
+        //Console.WriteLine("b jest wieksze od a:      A:" + a + "  B: " + b);
+    }
+}
+while (a == b & kontynuj == true)
+{
+    Console.WriteLine("a jest równe: " + a);
+    kontynuj = false;
+}
